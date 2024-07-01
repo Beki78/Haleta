1a36da1688358d895937e7b93a796aa805981197
Baki78 <bakiget78@gmail.com>
2024-07-01 19:26:05 +0300

    search bar added

--- client/app/(tabs)/home.jsx
+++ client/app/(tabs)/home.jsx
@@ -1,14 +1,24 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import "@expo/metro-runtime";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Ionicons } from "@expo/vector-icons";
+
 
 const Home = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView className="mx-8">
       <View>
-        <Text></Text>
+        <View className="flex flex-row gap-12 items-center">
+          <View className="border-[1px]  border-[#72B4BE] flex-[10]  flex flex-row p-2 rounded-md">
+            <FontAwesome name="search" size={24} color="#72B4BE" className="mx-3"/>
+            <TextInput className="flex-1 ml-3" placeholder="Search ambulance"/>
+          </View>
+          <View className="flex-[]">
+            <Ionicons name="options" size={35} color="#72B4BE" />
+          </View>
+        </View>
       </View>
     </SafeAreaView>
   );
